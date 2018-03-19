b1412b6b42374bf67f0004cb819f2a9553314868
Bas Pijls <b.pijls@hva.nl>
2018-03-19 21:16:58 +0100

    Stap 0: Initial Commit

---
+++ Asteroids/Asteroids.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Asteroids
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Asteroids : GameEnvironment
+    {       
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            screen = new Point(800, 600);
+            ApplyResolutionSettings();
+
+            // TODO: use this.Content to load your game content here
+        }
+
+
+    }
+}
--- Asteroids/Program.cs
+++ Asteroids/Program.cs
@@ -14,7 +14,7 @@ namespace Asteroids
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Asteroids())
                 game.Run();
         }
     }
